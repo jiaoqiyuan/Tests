e9d21ab853a6248d8d61ed3d857a5c4b68c409ef
jiaoqiyuan <jiaoqiyuan@outlook.com>
2019-07-08 11:39:25 +0800

    show output detaile, add sizeof char* int* double*

--- C/point_mem/point_mem.c
+++ C/point_mem/point_mem.c
@@ -6,11 +6,11 @@ int main(void)
 	int i = 99;
 	double x = 66.6;
 	float f = 0.1;
-	char * p = &ch;
-	int * q = &i;
-	double * r = &x;
+	char *p = &ch;
+	int *q = &i;
+	double *r = &x;
 
-	printf("%d %d %d\n", sizeof(p), sizeof(q), sizeof(r)); //分别输出不同类型的指针变量名占多少字节。
+	printf("sizeof(char*)=%d sizeof(int*)=%d sizeof(double*)=%d\n", sizeof(p), sizeof(q), sizeof(r)); //分别输出不同类型的指针变量名占多少字节。
 	printf("float = %d, double = %d\n", sizeof(f), sizeof(x));
 
 	return 0;
