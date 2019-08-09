b6a0a9de9eb35cd0c5d7c845094e3ff2c335c2e3
jiaoqiyuan <jiaoqiyuan@outlook.com>
2019-08-10 00:17:36 +0800

    使用外部函数malloc内存时需要注意参数传递的问题

---
+++ C/pointargs/pointargs.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void malloc_handle(char **handle) {
+    *handle = (char *)malloc(16);
+}
+
+void test_point_args() {
+    char *handle = NULL;
+    malloc_handle(&handle);
+    if (handle == NULL)
+    {
+        printf("handle is null\n");
+    } else
+    {
+        printf("handle is not null\n");
+    }
+    
+    
+}
+
+int main(int argc, char *argv[]) {
+    test_point_args();
+    return 0;
+}
