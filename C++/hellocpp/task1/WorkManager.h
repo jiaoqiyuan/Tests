a1af9b7609c48ac398aaf05ceadf5783cbf5ea84
jiaoqiyuan <jiaoqiyuan@outlook.com>
2020-09-15 21:10:46 +0800

    add task1

---
+++ C++/hellocpp/task1/WorkManager.h
@@ -0,0 +1,20 @@
+//
+// Created by Administrator on 2020/9/15.
+//
+
+#ifndef HELLOCPP_WORKMANAGER_H
+#define HELLOCPP_WORKMANAGER_H
+
+#include <iostream>
+
+using namespace std;
+
+class WorkManager {
+public:
+    WorkManager();
+    ~WorkManager();
+    static void show_menu();    // 显示菜单
+};
+
+
+#endif //HELLOCPP_WORKMANAGER_H
