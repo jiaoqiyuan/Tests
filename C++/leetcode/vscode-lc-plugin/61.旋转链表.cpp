123700b9e405a12169922338cceb775627fcf1b6
jiaoqiyuan <jiaoqiyuan@outlook.com>
2020-11-26 18:19:14 +0800

    旋转链表

---
+++ C++/leetcode/vscode-lc-plugin/61.旋转链表.cpp
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode.cn id=61 lang=cpp
+ *
+ * [61] 旋转链表
+ *
+ * https://leetcode-cn.com/problems/rotate-list/description/
+ *
+ * algorithms
+ * Medium (40.52%)
+ * Likes:    374
+ * Dislikes: 0
+ * Total Accepted:    98.6K
+ * Total Submissions: 243.2K
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * 给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 1->2->3->4->5->NULL, k = 2
+ * 输出: 4->5->1->2->3->NULL
+ * 解释:
+ * 向右旋转 1 步: 5->1->2->3->4->NULL
+ * 向右旋转 2 步: 4->5->1->2->3->NULL
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: 0->1->2->NULL, k = 4
+ * 输出: 2->0->1->NULL
+ * 解释:
+ * 向右旋转 1 步: 2->0->1->NULL
+ * 向右旋转 2 步: 1->2->0->NULL
+ * 向右旋转 3 步: 0->1->2->NULL
+ * 向右旋转 4 步: 2->0->1->NULL
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head || !head->next) return head;
+
+        int len = 0;
+        ListNode *cur = head;
+        ListNode *tail = NULL;
+        while(cur) {
+            len++;
+            cur = cur->next;
+            tail = !cur ? tail : cur;
+        }
+
+        int step = k % len;   //移动的步数
+        cur = head;
+        for (int i = 0; i < len - step - 1; i++) {
+            cur = cur->next;
+        }
+        tail->next = head;
+        head = cur->next;
+        cur->next = NULL;
+
+        return head;
+    }
+};
+// @lc code=end
+
