50ad22c588f49aabfb204889d032b1d0ba7ef528
jiaoqiyuan <jiaoqiyuan@outlook.com>
2020-11-26 11:27:50 +0800

    调整目录

---
+++ C++/leetcode/build/1290/CMakeFiles/leetcode1290.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leetcode1290.dir/leetcode1290.cpp.o"
+  "leetcode1290"
+  "leetcode1290.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leetcode1290.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C++/leetcode/build/offer18/CMakeFiles/offer18.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/offer18.dir/offer18.cpp.o"
+  "offer18"
+  "offer18.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/offer18.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
