2011580c1e0fc65f7b58c01dea48c3168b8f66b1
jiaoqiyuan <jiaoqiyuan@outlook.com>
2020-12-01 23:00:22 +0800

    添加反转链

---
+++ C++/leetcode/vscode-lc-plugin/92.反转链表-ii.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=92 lang=cpp
+ *
+ * [92] 反转链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        if (!head || !head->next || m == n) {
+            return head;
+        }
+
+        ListNode *dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode *pre = dummy;
+        for (int i = 1; i < m; i++) {
+            pre = pre->next;
+        }
+        head = pre->next;
+        for (int i = m; i < n; i++) {
+            ListNode *nxt = head->next;
+            head->next = nxt->next;
+            nxt->next = pre->next;
+            pre->next = nxt;
+        }
+        return dummy->next;
+    }
+};
+// @lc code=end
+
