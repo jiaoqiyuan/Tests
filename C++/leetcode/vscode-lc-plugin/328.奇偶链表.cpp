86a21c5281fc54d196e57e072da5f4afeb883015
jiaoqiyuan <jiaoqiyuan@outlook.com>
2020-12-03 09:39:04 +0800

    奇偶链表

--- C++/leetcode/vscode-lc-plugin/328.奇偶链表.cpp
+++ C++/leetcode/vscode-lc-plugin/328.奇偶链表.cpp
@@ -19,7 +19,56 @@ class Solution {
 public:
     // 可以使用尾插法解决
     ListNode* oddEvenList(ListNode* head) {
-        
+        // if (!head || !head->next || !head->next->next) return head;
+
+        // ListNode *cur = head;
+        // ListNode *tail = NULL;
+        // int len = 0;
+        // while(cur) {
+        //     len ++;
+        //     tail = cur;
+        //     cur = cur->next;
+        // }
+
+        // cur = head;
+        // for (int i = 1; i <= len; i++) {
+        //     if (i == len && len % 2 != 0) {
+        //         break;
+        //     } else if (i == len && len % 2 != 0) {
+        //         tail->next = cur;
+        //         cur->next = NULL;
+        //         tail = cur;
+        //         break;
+        //     }
+        //     ListNode *tmp = cur->next;
+        //     if (i % 2 != 0) {
+        //         cur->next = cur->next->next;
+        //     } else {
+        //         tail->next = cur;
+        //         cur->next = NULL;
+        //         tail = cur;
+        //     }
+        //     cur = tmp;
+        // }
+
+        // return head;
+
+        if(!head || !head->next) return head;
+        // 奇数尾指针
+        ListNode *odd_tail = head;
+        // 偶数头指针
+        ListNode *even_head = head->next;
+        // 偶数尾指针
+        ListNode *even_tail = even_head;
+
+        while(odd_tail->next && even_tail->next) {
+            odd_tail->next = even_tail->next;
+            odd_tail = even_tail->next;
+            even_tail->next = odd_tail->next;
+            even_tail = odd_tail->next;
+        }
+        odd_tail->next = even_head;
+        return head;
     }
 };
 // @lc code=end
