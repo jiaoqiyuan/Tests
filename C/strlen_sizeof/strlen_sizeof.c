cd5591f5e1a7d92d46f6e3c83c6e5418a75bdce4
jiaoqiyuan <jiaoqiyuan@outlook.com>
2020-12-05 18:08:15 +0800

    更新文件

--- C/strlen_sizeof/strlen_sizeof.c
+++ C/strlen_sizeof/strlen_sizeof.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
+/* 用于测试sizeof和strlen返回的结果
+ * sizeof用于计算数组分配的空间
+ * strlen用于计算数组占用的空间（不包含 '\n' 占用的空间）
+ */
+
 int main(int argc, char* argv[]) {
 	char str[10] = {0};
 	printf("sizeof str[10] = %d\n", sizeof(str));
