c2e5d3b92efe4e19606679abf161a475959aa915
jiaoqiyuan <jiaoqiyuan@outlook.com>
2020-12-09 14:19:37 +0800

    array test

--- C/array/const_char_array/const_string_contain.c
+++ C/array/const_char_array/const_string_contain.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
+
+/* 判断一个常量字符串数组中是否包含某个字符串 */
+
 const char *const BIG_DATA_SOURCES[2] = {
     "rrc03",
     "route-views3"
@@ -19,7 +22,15 @@ int if_contains(char *name) {
     return 0;
 }
 
+void test_array_init() {
+    char array[3] = {50, 60, 70};
+    for (int i = 0; i < 3; i++) {
+        printf("array[%d] = %c\n", i, array[i]);
+    }
+}
+
 int main() {
     if_contains("route-views3");
+    test_array_init();
     return 0;
 }
--- C/array/out_of_array_index/out_of_index.c
+++ C/array/out_of_array_index/out_of_index.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
-int main(int argc, char* argv[]){
+
+
+void test_array_index() {
 	int i = 0;
 	int arr[3] = {0};
 	for(; i<=3; i++){
 		arr[i] = 0;
 		printf("hello world\n");
 	}
+}
+
+/* 数组下标可以为负数 */
+void test2() {
+    double a1[4] = {1.2, 2.4, 3.6, 4.8};
+    a1[-1] = 20.5;
+    printf("a1[-1] = %f, addr = %p\n", a1[-1], &a1[-1]);
+    printf("al addr = %p\n", a1);
+}
+
+int main(int argc, char* argv[]){
+    //test_array_index();
+    test2();
 	return 0;
 }
+
