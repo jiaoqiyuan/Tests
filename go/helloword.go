93c6149a3a115be7c19560d679f4b4f64c6fe35b
jiaoqiyuan <jiaoqiyuan@outlook.com>
2021-02-25 11:53:41 +0800

    add go test

--- go/helloword.go
+++ go/helloword.go
@@ -2,7 +2,17 @@ package main
 
 import "fmt"
 
-func main() {
+func helloworld() {
 	fmt.Println("Hello World.")
 	fmt.Println("Hello" + "World.")
+}
+
+func main() {
+	// helloworld()
+
+	var a string = "Runoob"
+    fmt.Println(a)
+
+    b, c := 1, 2
+    fmt.Println(b, c)
 }
